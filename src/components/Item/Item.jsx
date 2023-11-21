fd35f2a9534ecc5f652db9b007ac2473653e0461
Maria8023 <mprmz@yahoo.com.ar>
2023-11-20 23:39:17 -0300

    2da pre entrega

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,17 @@
+import { useNavigate } from "react-router-dom"
+
+
+const Item = ({id, name, img, price}) => {
+    const navigate = useNavigate()
+
+    return (
+        <div>
+            <h3>{name}</h3>
+            <img src={img} style= {{width: 150}} />
+            <h3>${price}</h3>
+            <button onClick={() => navigate(`/detail/${id}`)}>Detalle</button>
+        </div>
+    )
+}
+
+export default Item
